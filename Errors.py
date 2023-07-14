b7927ddfafa78f09bb94b0580426300e8aabe720
Tornikekarelidz <138922419+Tornikekarelidz@users.noreply.github.com>
2023-07-14 23:30:58 +0400

    Add files via upload

---
+++ Errors.py
@@ -0,0 +1,50 @@
+name = Tornike"
+surname - 'Karelidze"
+age = "14
+height = 1. 65
+
+print(name+surname)
+
+actor = "Will Smith"
+producer = "Christopher Nolan"
+ 
+academy = "GOA"
+favshow = "Prison break"
+location = 'Sakartvelo,tbilisi"
+favfood - "Xinkali,Lobiani"
+favm oviecharacter = "manycharacters'
+
+print"location)
+
+fro m turtle impor t *
+
+skill - editin g
+skill 2 = programming
+
+print(skill)
+
+GoalOrientedAcademy = "joinbestacademy"
+joinGoa = "BecomeChad"
+
+print(joingoa + " " + GoalOrientedAcademy)
+
+everymemberinGOA = "GIGACHAD"
+
+favgame = "NOGAMESINGOA"
+lesson = "2shabati+5shabati"
+lessontime = 21:00
+
+print(lesson + " " + lessontime)
+
+from turtle import*
+speed(30)
+color("green")
+width(7)
+forward(300)
+left(90)
+forward(300)
+left(90)
+forward(300)
+left(90)
+forward(300)
+left(90)
